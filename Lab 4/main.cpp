14581dcbaf4a5856009d6522fe54ec709fbc54db
Sankalp Acharya <sankalpsanu2003@gmail.com>
2024-06-02 10:23:15 +0545

    second

--- Lab 4/main.cpp
+++ Lab 4/main.cpp
@@ -1,5 +1,17 @@
 #include<iostream>
 
-int main(){
-    
+int main()
+{
+
+	int arr[] = {1,3,5,6,9,11 };
+	int n = 6;
+
+	quickSort(arr, 0, n - 1);
+
+	for (int i = 0; i < n; i++)
+     {
+		cout << arr[i] << " ";
+	}
+
+	return 0;
 }
