a0db496be81189866540b8abc85e8c44bda9a6b6
Hridayanshu Raj Acharya <ha03228022@student.ku.edu.np>
2024-06-06 18:52:18 +0545

    time function added

--- Lab_4/main.cpp
+++ Lab_4/main.cpp
@@ -1,16 +1,18 @@
 #include<iostream>
 #include <random> // for std::mt19937
+// #include <ctime> // for clock(), clock_t, and CLOCKS_PER_SEC
+#include <chrono> // for high_resolution_clock
 #include"Insertion_Sort/Include/insertion.h"
 #include"Quick_Sort/Include/quick.h"
 int main()
 {   //insertion sort 
-    int array_size = 10;
+    int array_size = 1000;
     //Seed with real random value
     std::random_device rd;
     //Initialize Mersenne Twister pseudo-random number generator
     std::mt19937 gen(rd());
     //Defining the range for random numbers
-    std::uniform_int_distribution<>dis(0,100);
+    std::uniform_int_distribution<>dis(0,1000);
     int random_numbers[array_size];
     //Generating random numbers
     for(int i=0;i<array_size; i++){
@@ -19,17 +21,25 @@ int main()
     //Generated random numbers
     std::cout << "Generated random numbers:\n ";
     printArray(random_numbers, array_size);
+    
+    // Measure time taken by insertion sort
+    auto start = std::chrono::high_resolution_clock::now();
     //passing generated random numbers to insertion sort function
     insertionSort(random_numbers, array_size);
-    cout<<"Sorted Array using insertion sort:\n";
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> insertion_sort_duration = end - start;
+    cout<<"\nSorted Array using insertion sort:\n";
     printArray(random_numbers,array_size);
-    //Sorted array for quick sort
-	int arr[] = {0,25,34,38,45,49,55,66,77,99 };
-    cout<<"Array passed to quick sort function:\n";
-    printArray(arr,array_size);
-    //Passing the sorted array in quick sort function
-	quickSort(arr, 0, array_size - 1);
-    //sorted array
-    cout<<"Sorted Array using quick sort:\n";
-    printArray(arr,array_size);
+    std::cout << "Time taken by insertion sort: " << insertion_sort_duration.count() << " milliseconds\n";
+   
+    //passing the sorted array from insertion sort to quick sort and calculating time to sort
+    start = std::chrono::high_resolution_clock::now();
+    quickSort(random_numbers, 0, array_size - 1);
+    end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> quick_sort_duration = end - start;
+
+    // Print sorted array using quick sort
+    std::cout << "\n\nSorted array using quick sort:\n";
+    printArray(random_numbers, array_size);
+    std::cout << "Time taken by quick sort: " << quick_sort_duration.count() << " milliseconds\n";
 }
